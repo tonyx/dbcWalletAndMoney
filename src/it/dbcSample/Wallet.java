b8a023395c254c025ab3ce2763157e664b6bfd2d
Tonino Lucca <tonyx1@gmail.com>
2013-02-22 20:20:49 +0100

    refactoring packages

---
+++ src/it/dbcSample/Wallet.java
@@ -0,0 +1,54 @@
+package it.dbcSample;
+
+import io.ConsoleOutput;
+import io.StreamOutput;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tony
+ * Date: 11/02/13
+ * Time: 15.11
+ * To change this template use File | Settings | File Templates.
+ */
+public class Wallet {
+    StreamOutput streamOutput;
+    Money money;
+
+    public static void main(String string[]){
+        Wallet wallet = new Wallet(new ConsoleOutput());
+        wallet.run();
+    }
+
+    public Wallet(StreamOutput streamOutput) {
+        this(streamOutput, new Money(0));
+    }
+
+    private Wallet(StreamOutput streamOutput, Money money) {
+        this.streamOutput= streamOutput;
+        this.money=money;
+    }
+
+    public void run() {
+        sayWelcome();
+    }
+
+    private void sayWelcome() {
+        streamOutput.output("welcome to your wallet");
+    }
+
+    public void addMoney(int addedmoney)  {
+        money = money.addAmount(addedmoney);
+        streamOutput.output("you just added "+addedmoney+" to your wallet");
+    }
+
+    public void spendMoney(int spentAmount) {
+        if (spentAmount> money.getAmount()) {
+            streamOutput.output("insufficient credit");
+        } else {
+            money = money.spendMoney(spentAmount);
+            streamOutput.output("you just spent "+spentAmount);
+        }
+    }
+}
+
+
---
+++ src/test/it/dbcSample/WalletTest.java
@@ -0,0 +1,81 @@
+package test.it.dbcSample;
+
+import io.StreamOutput;
+import it.dbcSample.Wallet;
+import org.junit.Test;
+import org.mockito.InOrder;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tony
+ * Date: 11/02/13
+ * Time: 16.23
+ * To change this template use File | Settings | File Templates.
+ */
+public class WalletTest {
+    @Test
+    public void shouldSayWelcome() {
+        // given
+        StreamOutput streamOutput = mock(StreamOutput.class);
+
+        // when
+        Wallet wallet = new Wallet(streamOutput);
+        wallet.run();
+
+        // then
+        verify(streamOutput).output("welcome to your wallet");
+    }
+
+    @Test
+    public void shouldPutSomeInitialMoneyInTheWallet() {
+        // given
+        StreamOutput streamOutput = mock(StreamOutput.class);
+
+        // when
+        Wallet wallet = new Wallet(streamOutput);
+        wallet.run();
+        wallet.addMoney(10);
+
+        // then
+        verify(streamOutput).output("welcome to your wallet");
+        verify(streamOutput).output("you just added 10 to your wallet");
+    }
+
+    @Test
+    public void canSpendMoneyTherAreAvailable() {
+        // given
+        StreamOutput streamOutput = mock(StreamOutput.class);
+
+        // when
+        Wallet wallet = new Wallet(streamOutput);
+        wallet.run();
+        wallet.addMoney(1);
+        wallet.spendMoney(1);
+        InOrder inOrder = inOrder(streamOutput);
+
+        // then
+        inOrder.verify(streamOutput).output("welcome to your wallet");
+        inOrder.verify(streamOutput).output("you just added 1 to your wallet");
+        inOrder.verify(streamOutput).output("you just spent 1");
+    }
+
+    @Test
+    public void walletCantSpendMoneyThatDoesNotHave() {
+        // given
+        StreamOutput streamOutput = mock(StreamOutput.class);
+
+        // when
+        Wallet wallet = new Wallet(streamOutput);
+        wallet.run();
+        wallet.addMoney(1);
+        wallet.spendMoney(2);
+        InOrder inOrder = inOrder(streamOutput);
+
+        // then
+        inOrder.verify(streamOutput).output("welcome to your wallet");
+        inOrder.verify(streamOutput).output("you just added 1 to your wallet");
+        inOrder.verify(streamOutput).output("insufficient credit");
+    }
+}
